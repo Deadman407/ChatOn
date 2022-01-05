8e9740cfc41137bde7b4f0da7a3d6e1bf3310d41
Deadman407 <dead.407.man@gmail.com>
2022-01-05 06:58:13 -0800

    Added Join page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Chat from './components/Chat/Chat'
+import Join from './components/Join/Join'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Join />} />
+        <Route path='/chat' element={<Chat />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
