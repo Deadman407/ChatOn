c5ac2f175a0c003942734182a6641c9bd6ec88ab
Deadman407 <dead.407.man@gmail.com>
2022-01-06 01:52:41 -0800

    Added socket.io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const socketio = require('socket.io')
 const http = require('http')
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 const PORT = process.env.PORT || 5000
 
@@ -8,10 +9,28 @@ const router = require('./router')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!')
+
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room })
+
+        if(error) return callback(error)
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}` })
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!` })
+
+        socket.join(user.room)
+
+        callback()
+    })
 
     socket.on('disconnect', () => {
         console.log("User has left!!!")
